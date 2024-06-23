1aa6ca34df83ae61cb56c09f32195d6a9d9a7c3a
Amangeldi256 <139152045+Amangeldi256@users.noreply.github.com>
2024-06-23 13:50:28 +0500

    Update Planet.java

--- src/org/company/Planet.java
+++ src/org/company/Planet.java
@@ -5,30 +5,10 @@ public class Planet {
         System.out.println("Resolve method");
     }
 
-    void cry() {
-        System.out.println("Duck cry cry");
-    }
-
-    void mashina() {
-        System.out.println("vyun vun vun");
-    }
-
-    void chicken() {
-        System.out.println("kukarya ku");
-    }
-
     public static void main(String[] args) {
         Planet res = new Planet();
         res.resolve();
 
-        Planet duck = new Planet();
-        duck.cry();
-
-
-        Planet mash = new Planet();
-        mash.mashina();
-
-        Planet chik = new Planet();
-        chik.chicken();
+    
     }
 }
