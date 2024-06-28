947e0671e6fcbc2b61084f76ff51279aba4b644e
Aman <ortokorganic@gmail.com>
2024-06-28 21:54:14 +0600

    ExerciseTen

---
+++ src/org/company/ExerciseTen/Ten.java
@@ -0,0 +1,10 @@
+package org.company.ExerciseTen;
+
+public class Ten {
+    public static void main(String[] args) {
+        System.out.println("RUN");
+        System.out.println("RUN");
+        System.out.println("RUN");
+        System.out.println("RUN");
+    }
+}
