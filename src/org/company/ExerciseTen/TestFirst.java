e6f2af87fbf389252c69666a4b99720c9057bfc3
Aman <ortokorganic@gmail.com>
2024-07-14 18:15:45 +0600

    Second Abc

--- src/org/company/ExerciseTen/TestFirst.java
+++ src/org/company/ExerciseTen/TestFirst.java
@@ -6,42 +6,45 @@ import java.util.Scanner;
 
 public class TestFirst {
     public static void main(String[] args) throws FileNotFoundException {
-
         //File file = new File(str);
 
-        File file = new File("D:\\Udemi\\first\\01.txt");
-        //File file = new File("Udemi\\first\\02.txt");
+        // File file = new File("D:\\Udemi\\first\\01.txt");
+        File file = new File("D:\\Udemi\\first\\02.txt");
         Scanner scanner = new Scanner(file);
 
         String str = scanner.nextLine();
 
+        // Преобразование строки в массив символов
+        char[] charArray = str.toCharArray();
+        int count=1;
+        char c[] = {
+                'a', 'b', 'c', 'd',
+                'a', 'b', 'c', 'd',
+                'a', 'b', 'c', 'd',
+                'a', 'b', 'c', 'd',
+                'a', 'b', 'c', 'd',
+                'a', 'b', 'c', 'd'
+        };
 
-        char[] letters = new char[52];
-        int index = 0;
+  //      int[] array = new int[charArray.length];
+        // Вывод массива символов
 
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            letters[index++] = ch;
-        }
 
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
-            letters[index++] = ch;
-        }
-
-        int[] counter = new int[52];
-
-        for (char element : str.toCharArray()) {
-            for (int i = 0; i < letters.length; i++) {
-                if (element == letters[i]) {
-                    counter[i]++;
+            for (int i = 0; i < charArray.length; i++) {
+                if(Character.isLetter(charArray[i]))
+                System.out.println(" " + count + " " + charArray[i]);
                 }
-            }
-        }
+        System.out.println("");
 
-        for (int i = 0; i < letters.length; i++) {
-            if (counter[i] > 0) {
-                System.out.print(letters[i] + "" + counter[i] + "; ");
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] >= 65 && charArray[i] <= 122) {
+                System.out.println(" " + count + " " + charArray[i]);
+                count++;
             }
         }
-        System.out.println();
+        System.out.println("");
+
+            }
     }
-}
+
--- src/org/company/ExerciseTen/TestSecond.java
+++ src/org/company/ExerciseTen/TestSecond.java
@@ -1,23 +1,46 @@
 package org.company.ExerciseTen;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TestSecond {
-    public static void main(String[] args) {
-        char r = 15;
-        System.out.println(r);
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("D:\\Udemi\\first\\02.txt");
+        Scanner scanner = new Scanner(file);
+        String str = scanner.nextLine();
+        // Преобразование строки в массив символов
+        char[] charArray = str.toCharArray();
+        int count=0;
+        char[] array = new char[26];
+        int index = 0;
 
-        char s = 30;
-        System.out.println(s);
-        char m = 60;
-        System.out.println(m);
-        char e = 85;
-        System.out.println(e);
+            for (int i = 0; i < charArray.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if(charArray[i] == c) {
+                        array[index++] = c;
+                        count++;
+                        System.out.print(" " + count + " " + charArray[i]);
+                }
+            }
 
-        int start = 65;
-        int thend = 123;
-        char[] letters = new char[thend];
-
-        for (int i = 0; i <= thend; i++) {
-            letters[i] = start;
         }
+        System.out.println();
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < charArray.length; i++) {
+                array[index] = c;
+                if(charArray[i] == c)
+                    count++;
+                System.out.print(" " + count + " " + charArray[i]);
+
+            }
+
+            // System.out.print(c + " ");
+        }
+        System.out.println();
+
+
+
     }
 }
